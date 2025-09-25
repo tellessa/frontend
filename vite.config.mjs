7b4e134abacdd8bc443ad852a00c1f4c3c4dbbd4
tellessa <76462081+tellessa@users.noreply.github.com>
2025-09-24 21:53:02 -0700

    Gets app working

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,9 +1,10 @@
+
+// This file should be renamed to vite.config.mjs for ESM compatibility
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',
   build: {
     outDir: 'dist',
   },
